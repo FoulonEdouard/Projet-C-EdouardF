d7d9f36f61cd8ba4e478a35f7c252878087058f8
FoulonEdouard <140059867+FoulonEdouard@users.noreply.github.com>
2023-08-01 10:02:00 +0200

    Add files via upload

---
+++ CMakeFiles/pie.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pie.dir/main.c.o"
+  "pie"
+  "pie.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/pie.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
