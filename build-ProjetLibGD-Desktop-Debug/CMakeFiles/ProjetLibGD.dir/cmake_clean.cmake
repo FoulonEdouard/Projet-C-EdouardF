801021a7df82b8932cc50f24ef4e38f717afc6f9
FoulonEdouard <140059867+FoulonEdouard@users.noreply.github.com>
2023-08-01 10:58:31 +0200

    Add files via upload

---
+++ build-ProjetLibGD-Desktop-Debug/CMakeFiles/ProjetLibGD.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ProjetLibGD.dir/main.c.o"
+  "ProjetLibGD"
+  "ProjetLibGD.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/ProjetLibGD.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
